5c63c4994f76e6f97893fe5e38b5e1d44038b797
belousovko <working.belousovko@gmail.com>
2021-01-04 19:51:43 +0700

    lesson 13

---
+++ src/components/Nav.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+const Nav = () => {
+    return <nav className={'nav'}>
+        <div>Home</div>
+        <div>Messages</div>
+    </nav>
+}
+
+export default Nav;
---
+++ src/components/Profile.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+const Profile = () => {
+    return <div className={'content'}>
+        <img
+            src={"https://image.shutterstock.com/image-photo/mountains-under-mist-morning-amazing-260nw-1725825019.jpg"}/>
+        <div>
+            <div>post 1</div>
+            <div>post 2</div>
+            <div>post 3</div>
+        </div>
+    </div>
+}
+
+export default Profile;
