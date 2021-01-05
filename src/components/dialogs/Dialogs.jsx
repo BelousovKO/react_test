7eb6eccc69351ef2d2376e66729f2bae89d62420
belousovko <working.belousovko@gmail.com>
2021-01-05 20:31:21 +0700

    lesson 21

--- src/components/dialogs/Dialogs.jsx
+++ src/components/dialogs/Dialogs.jsx
@@ -3,8 +3,32 @@ import s from './Dialogs.module.css';
 
 const Dialogs = (props) => {
     return (
-        <div>
-            Dialogs
+        <div className={s.dialogs}>
+            <div className={s.dialogItems}>
+                <div className={s.dialog + ' ' + s.active}>
+                    Olga
+                </div>
+                <div className={s.dialog}>
+                    Polina
+                </div>
+                <div className={s.dialog}>
+                    Mihail
+                </div>
+                <div className={s.dialog}>
+                    Max
+                </div>
+            </div>
+            <div className={s.messages}>
+                <div className={s.message}>
+                    Hi
+                </div>
+                <div className={s.message}>
+                    Hello
+                </div>
+                <div className={s.message}>
+                    Bi
+                </div>
+            </div>
         </div>
     )
 }
